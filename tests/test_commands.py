0410c58f071d81a645bd5b464eb58635679a8dd9
Immortalsonly <remco.eijsackers@gmail.com>
2023-12-16 23:42:26 +0100

    updated command tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,21 +2,27 @@ import re
 from unittest import TestCase
 
 from src.commands import generate_account
+from src.commands.command_handler import CliAccountHandler
+
 
 class TestCommands(TestCase):
 
 
     def test_it_returns_the_correct_type(self):
 
-        test_agents = generate_account()
+        args = CliAccountHandler.handle_request({})
+
+        test_agents = generate_account(**args)
 
         self.assertIsInstance(test_agents, list)
         self.assertIsInstance(test_agents[0], dict)
 
 
     def test_it_returns_the_expected_values(self):
+        
+        args = CliAccountHandler.handle_request({})
 
-        test_agents = generate_account()
+        test_agents = generate_account(**args)
 
         keys = ["first_name", "last_name", "birthdate", "email", "password", "nationality", "birthdate", "age"]
 
